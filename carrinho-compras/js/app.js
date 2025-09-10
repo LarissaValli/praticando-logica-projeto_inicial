866b33d3fd49d34e96c6c71d7e0b08d6cad623a8
Larissa Valli <larissavalli4@gmail.com>
2025-09-10 14:00:16 -0300

    desafio carrinho de compra

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -24,3 +24,17 @@ function limpar() {
     document.getElementById('lista-produtos').innerHTML = '';
     document.getElementById('valor-total').textContent = 'R$0';
 }
+
+// Vamos praticar?
+
+// Adicione validações no código para garantir que o usuário só consiga adicionar produtos ao carrinho se tiver selecionado um produto válido e inserido uma quantidade válida. Se a entrada não for válida, exiba uma mensagem de erro apropriada.
+
+// Crie um formulário simples com campos de entrada e utilize JavaScript para recuperar os valores digitados nos campos usando a propriedade value. Exiba os valores no console e na tela.
+
+// Tenha um elemento HTML na página (por exemplo, um parágrafo) e utilize JavaScript para modificar seu conteúdo usando a propriedade textContent.
+
+// Faça a soma de duas variáveis numéricas e apresente no console com uma mensagem amigável em Template String.
+
+// Receba, no mínimo, duas sentenças como uma string e utilize split() para quebrá-la em frases menores com base em um ponto e vírgula como delimitador.
+
+// Receba uma string contendo números separados por vírgulas. Utilize split() para separar os números e exibi-los no console.
