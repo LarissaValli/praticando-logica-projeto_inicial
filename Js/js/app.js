556a5b799fa5a816b8a3ca060c01353896c4b241
Larissa Valli <larissavalli4@gmail.com>
2025-09-16 11:46:17 -0300

    aprendendo o basico com yt

--- Js/js/app.js
+++ Js/js/app.js
@@ -1,3 +1,9 @@
 //Perguntando qual o nome da pessoa e dar boas vindas
-alert('Olá mundo!');
-let nome = 
+
+//Alert é para mostrar só uma mensagem.
+//alert('Olá mundo!');
+//Prompt é um comando para fazer uma pergunta.
+//let armazenar um valor na máquina com o nome da variavel.
+let nome = prompt('Qual seu nome?');
+alert(`Seja bem vindo(a), ${nome}`);
+
