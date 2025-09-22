89935968479e28df4a29cfe34a1479dfdc0ddd14
Larissa Valli <larissavalli4@gmail.com>
2025-09-22 17:31:29 -0300

    exercicio do livro js

--- Js/js/app.js
+++
@@ -1,9 +0,0 @@
-//Perguntando qual o nome da pessoa e dar boas vindas
-
-//Alert é para mostrar só uma mensagem.
-//alert('Olá mundo!');
-//Prompt é um comando para fazer uma pergunta.
-//let armazenar um valor na máquina com o nome da variavel.
-let nome = prompt('Qual seu nome?');
-alert(`Seja bem vindo(a), ${nome}`);
-
