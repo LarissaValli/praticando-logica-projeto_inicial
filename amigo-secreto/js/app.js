aa16a839ffb72a1d83d57a55584bdea58de9f722
Larissa Valli <larissavalli4@gmail.com>
2025-09-26 11:51:35 -0300

    Finalizado amigo secreto com alura

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,12 +1,69 @@
+let amigos = [];
+
 function adicionar() {
-    let amigo = document.getElementById('nome-amigo');
-    let lista = document.getElementById('lista-amigos');
-    
+    let amigo = document.getElementById("nome-amigo");
+    let lista = document.getElementById("lista-amigos");
+    amigos.push(amigo.value);
     if(lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
-        lista.textContent = lista.textContent + ', ' + amigo.value;
+        lista.textContent = lista.textContent + ',' + amigo.value;
     }
-
     amigo.value = '';
 }
+
+function sortear() {
+    embaralha(amigos);
+    let sorteio = document.getElementById("lista-sorteio");
+
+    for(let i = 0; i < amigos.length; i++) {
+
+        if(i == amigos.length - 1) {
+           sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>';
+        } else {
+             sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i + 1] + '<br>';
+        }
+    }
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
+
+//EXERCÍCIO ALURA
+
+// Crie uma função que valide se um número é positivo, negativo ou zero.
+
+// Implemente uma função que verifique se uma pessoa é maior de idade.
+
+// Desenvolva uma função que valide se uma string é vazia ou não.
+
+// Crie uma função que determine se um ano é bissexto
+
+// Implemente uma função que calcule a média de dois números, interrompendo a execução se algum dos números não for válido.
+
+// Desenvolva uma função que receba um array como parâmetro e retorne o seu tamanho.
+
+// Crie um array e utilize a função includes para verificar se um elemento específico está presente
+
+// Implemente uma função que receba um array e um elemento, e retorne se o elemento está presente no array.
+
+// Crie um array de strings e utilize includes para verificar se uma determinada string está presente.
+
+// Desenvolva uma função que receba um array de objetos representando estudantes de um curso e um objeto que representa um estudante procurado. Depois retorne se o objeto está presente no array.
+
+// Crie uma função que receba um array de números e retorne a soma dos elementos pares e o produto dos elementos ímpares.
